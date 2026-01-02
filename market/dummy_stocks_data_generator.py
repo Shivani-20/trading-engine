80892927e433ea045486875b94eb2f0bc6d252ce
Shivani-20 <sinhashivani046@gmail.com>
2026-01-03 00:42:21 +0530

    removed env file and updated code to fetch env variables from container

--- market/dummy_stocks_data_generator.py
+++ market/dummy_stocks_data_generator.py
@@ -3,28 +3,23 @@ from datetime import datetime
 import random
 from core.events import Event, EventType
 import os
-from dotenv import load_dotenv
-
-# Load variables from .env file
-load_dotenv()
-
 
 class Generator:
     def __init__(self, event_bus):
         self.event_bus = event_bus
-        self.price = int(os.getenv("DEFAULT_PRICE"))
+        self.price = int(os.environ("DEFAULT_PRICE"))
 
     async def start(self):
         while True:
-            self.price += random.randint(-int(os.getenv("PRICE_FLUCTUATION")), int(os.getenv("PRICE_FLUCTUATION")))
+            self.price += random.randint(-int(os.environ("PRICE_FLUCTUATION")), int(os.environ("PRICE_FLUCTUATION")))
             current_time = datetime.now()
             tick = {
-                "instrument": os.getenv("DEFAULT_INDEX"),
+                "instrument": os.environ("DEFAULT_INDEX"),
                 "price": self.price,
                 "time": current_time.strftime("%H:%M:%S")
             }
 
-            if current_time.time() >= datetime.strptime(os.getenv("MARKET_CLOSE"), "%H:%M").time():
+            if current_time.time() >= datetime.strptime(os.environ("MARKET_CLOSE"), "%H:%M").time():
                 await self.event_bus.publish(
                     Event(EventType.SHUTDOWN, {})
                 )
